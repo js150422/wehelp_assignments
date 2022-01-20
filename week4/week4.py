b047cf3966b4b9c852d2acddd041e19a9a689326
Jane <91937175+js150422@users.noreply.github.com>
2022-01-20 09:07:04 +0800

    Add files via upload

---
+++ week4/week4.py
@@ -0,0 +1,46 @@
+from flask import Flask # 載入 Flack
+from flask import request # 載入 Request 物件
+from flask import render_template # 載入render_template函式
+from flask import redirect # 載入 redirect
+from flask import session
+app=Flask(__name__, static_url_path="/") 
+
+app.secret_key="any string but secret" # 設定 Session的密鑰
+# 網站的首頁
+@app.route("/")
+def index():
+    return render_template("week4.html")
+
+
+@app.route("/signin", methods=["POST"])
+def singnin():
+    account=request.form["account"]
+    key=request.form["key"]
+    if account=="test" and key=="test":
+        session["account"]=account # session["欄位名稱"]=資料
+        return redirect("/member")
+    else:
+        return redirect("/error?msg=帳號或密碼輸入錯誤")
+    
+@app.route("/member")
+def member():
+    account=session.get("account")
+    if "account" in session:
+        return render_template("member.html",account=account)
+    else:
+        return redirect("/")
+@app.route("/signout")
+def singout():
+    out=session.clear()
+    return redirect("/")
+
+
+
+@app.route("/error")
+def error():
+    message=request.args.get("msg","帳號、或密碼輸入錯誤")
+    return render_template("error.html",message=message)
+    
+   
+# 啟動伺服器在 Port 3000
+app.run(port=3000) 
