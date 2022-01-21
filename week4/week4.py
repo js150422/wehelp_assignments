da2698101a67b20f6d69a5249cdeedf16dc8b544
Jane <91937175+js150422@users.noreply.github.com>
2022-01-21 08:01:27 +0800

    Update week4.py

--- week4/week4.py
+++ week4/week4.py
@@ -16,11 +16,15 @@ def index():
 def singnin():
     account=request.form["account"]
     key=request.form["key"]
-    if account=="test" and key=="test":
-        session["account"]=account # session["欄位名稱"]=資料
-        return redirect("/member/")
+    if account!="" and key!="":
+        if account=="test" and key=="test":
+            session["account"]=account # session["欄位名稱"]=資料
+            return redirect("/member/")
+        else:
+            return redirect("/error/?msg=帳號或密碼輸入錯誤")
     else:
-        return redirect("/error/?msg=帳號或密碼輸入錯誤")
+         return redirect("/error/?msg=請輸入帳號密碼")
+       
     
 @app.route("/member/")
 def member():
@@ -38,8 +42,12 @@ def singout():
 
 @app.route("/error/")
 def error():
-    message=request.args.get("msg","帳號、或密碼輸入錯誤")
-    return render_template("error.html",message=message)
+    if "account" not in session:
+        message=request.args.get("msg","請輸入帳號、密碼")
+        return render_template("error.html",message=message)
+    else:
+        message=request.args.get("msg","帳號、或密碼輸入錯誤")
+        return render_template("error.html",message=message)
     
    
 # 啟動伺服器在 Port 3000
